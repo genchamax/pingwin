3ca1713368906d06368972436ac60bc0442b0d71
genchamax <cool.mr.maksim@gmail.com>
2017-05-30 22:54:20 +0300

    Add create article component

---
+++ src/app/components/blog/article/create-article.component.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by Max on 29.05.2017.
+ */
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  templateUrl: "./create-article.component.html",
+  styleUrls: ["./create-article.component.css"]
+})
+
+export class CreateArticleComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    //FIXME: Write code here
+  }
+}
