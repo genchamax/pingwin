7e2c27889700766adaf887e95b7e9cd2826aeae0
genchamax <cool.mr.maksim@gmail.com>
2017-06-05 03:34:47 +0300

    Add article preview item component

---
+++ src/app/components/blog/article-preview/article-preview.component.ts
@@ -0,0 +1,28 @@
+/**
+ * Created by Max on 04.06.2017.
+ */
+import {Component, Input, OnInit} from "@angular/core";
+import {Article} from "../../../models/article/article";
+import {BlogService} from "../blog.service";
+import {ArticlePreview} from "../../../models/article/article-preview";
+
+@Component({
+  moduleId: module.id,
+  selector: "article-preview",
+  templateUrl: "./article-preview.component.html",
+  styleUrls: ["./article-preview.component.css"]
+})
+export class ArticlePreviewComponent implements OnInit {
+  @Input() article: Article;
+
+  private blogService: BlogService;
+  private articlePreview: ArticlePreview;
+
+  constructor(blogService: BlogService) {
+    this.blogService = blogService;
+  }
+
+  ngOnInit(): void {
+    this.articlePreview = this.blogService.toArticlePreview(this.article);
+  }
+}
