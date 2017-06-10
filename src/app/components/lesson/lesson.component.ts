14325d06517d7c2caf4b374dffa5dfd14c49e3e2
genchamax <cool.mr.maksim@gmail.com>
2017-06-10 23:48:01 +0300

    Add lesson component

--- src/app/components/lesson/lesson.component.ts
+++ src/app/components/lesson/lesson.component.ts
@@ -1,3 +1,16 @@
 /**
  * Created by Max on 10.06.2017.
  */
+
+import {Component, Input} from "@angular/core";
+import {Lesson} from "../../domain/lesson";
+
+@Component({
+  moduleId: module.id,
+  selector: 'pingwin-lesson',
+  templateUrl: './lesson.component.html',
+  styleUrls: ['./lesson.component.css']
+})
+export class LessonComponent {
+  @Input() lesson: Lesson;
+}
