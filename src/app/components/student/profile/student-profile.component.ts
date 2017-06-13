44434e84bd60eb177c1e66518a66fe8e57063fec
olesiailchuk <olesia.ilchuk@agrocore.com.ua>
2017-06-13 07:24:53 +0300

    add dictionary acordion

--- src/app/components/student/profile/student-profile.component.ts
+++ src/app/components/student/profile/student-profile.component.ts
@@ -2,6 +2,7 @@
  * Created by LWells on 29.05.2017.
  */
 import {Component} from '@angular/core';
+import {ModalDismissReasons, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 
 @Component({
@@ -11,4 +12,26 @@ import {Component} from '@angular/core';
 })
 
 export class StudentProfileComponent {
+
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
